d7ad29da8c5845b89a2c7dd1662894fe4290db4e
Perry Gee <pgee@physics.ucdavis.edu>
2014-05-15 19:36:11 -0500

    Delete the comparison tests and move to MeasComparison.git

--- examples/processCcd0.py
+++
@@ -1,9 +0,0 @@
-from lsst.meas.base.sfm import SingleFrameMeasurementTask
-root.tableVersion=0
-root.measurement.algorithms = ["shape.sdss", "flux.gaussian", "flux.psf", "flux.sinc", "flux.naive", "skycoord", "classification.extendedness","flags.pixel"]
-root.measurement.slots.centroid = "centroid.sdss"
-root.measurement.slots.shape = "shape.sdss"
-root.measurement.slots.apFlux = "flux.sinc"
-root.measurement.slots.modelFlux = "flux.gaussian"
-root.measurement.slots.psfFlux = "flux.psf"
-root.measurement.slots.instFlux = "flux.naive"
