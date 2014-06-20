f83363a7a568259ec14ac3804ff072e4974f9907
Perry Gee <pgee@physics.ucdavis.edu>
2014-06-19 20:28:03 -0500

    temporarily reimplement schema prefix.

--- python/lsst/meas/base/base.py
+++ python/lsst/meas/base/base.py
@@ -300,11 +300,17 @@ class SourceSlotConfig(lsst.pex.config.Config):
 class BaseMeasurementConfig(lsst.pex.config.Config):
     """Baseconfig class for all measurement driver tasks."""
 
-    prefix = None
+    #prefix = None
 
     slots = lsst.pex.config.ConfigField(
         dtype = SourceSlotConfig,
         doc="Mapping from algorithms to special aliases in Source."
+        )
+
+    prefix = lsst.pex.config.Field(
+        dtype = str,
+        doc="Prefix to field name - DELETE THIS.",
+        default = ""
         )
 
     doReplaceWithNoise = lsst.pex.config.Field(dtype=bool, default=True, optional=False,
