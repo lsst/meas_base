bf1a011de257acc306905d427c8943e3dea94e60
Jim Bosch <jbosch@astro.princeton.edu>
2014-10-08 18:32:35 -0400

    Header for flag utilities

---
+++ include/lsst/meas/base/FlagHandler.h
@@ -0,0 +1,69 @@
+// -*- lsst-c++ -*-
+/*
+ * LSST Data Management System
+ * Copyright 2008-2013 LSST Corporation.
+ *
+ * This product includes software developed by the
+ * LSST Project (http://www.lsst.org/).
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the LSST License Statement and
+ * the GNU General Public License along with this program.  If not,
+ * see <http://www.lsstcorp.org/LegalNotices/>.
+ */
+
+#ifndef LSST_MEAS_BASE_FlagHandler_h_INCLUDED
+#define LSST_MEAS_BASE_FlagHandler_h_INCLUDED
+
+#include <vector>
+
+#include "lsst/afw/table/Schema.h"
+#include "lsst/afw/table/BaseRecord.h"
+#include "lsst/meas/base/exceptions.h"
+
+namespace lsst { namespace meas { namespace base {
+
+/**
+ *  @brief Simple POD struct used to define and document flags
+ *
+ *  When we switch to C++11, we can make the attributes std::strings, but at least for now we'll use
+    C-strings so we can create these arrays using initializer lists even in C++98.
+ */
+struct FlagDefinition {
+    char const * name;
+    char const * doc;
+};
+
+class FlagHandler {
+public:
+
+    explicit FlagHandler(
+        FlagDefinition const * begin,
+        FlagDefinition const * end
+    );
+
+    bool get(afw::table::BaseRecord const & record, int i) const;
+
+    void set(afw::table::BaseRecord & record, int i, bool value) const;
+
+    void fail(afw::table::BaseRecord & record, MeasurementError const * error=NULL) const;
+
+private:
+
+    typedef std::vector< afw::table::Key<afw::table::Flag> > FlagKeyVector;
+
+    FlagKeyVector _keys;
+};
+
+}}} // lsst::meas::base
+
+#endif // !LSST_MEAS_BASE_FlagHandler_h_INCLUDED
