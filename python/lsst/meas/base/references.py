75b5005f4f70c52280f3bbbfdc021718c13763b1
Paul Price <price@astro.princeton.edu>
2014-12-11 00:29:38 -0500

    CoaddSrcReferencesTask: make the dataset suffix a class variable
    While we may want to load from deepCoadd_src, under the multiband
    processing scheme it's deepCoadd_ref, so that suffix needs to be
    customisable.  Have NOT gone to deepCoadd_ref exclusively yet, so the
    old forced photometry should still work.

--- python/lsst/meas/base/references.py
+++ python/lsst/meas/base/references.py
@@ -160,6 +160,7 @@ class CoaddSrcReferencesTask(BaseReferencesTask):
     """
 
     ConfigClass = CoaddSrcReferencesConfig
+    datasetSuffix = "src" # Suffix to add to "Coadd_" for dataset name
 
     def __init__(self, butler=None, schema=None, **kwargs):
         """! Initialize the task.
@@ -187,7 +188,7 @@ class CoaddSrcReferencesTask(BaseReferencesTask):
 
         The given dataRef must include the tract in its dataId.
         """
-        dataset = self.config.coaddName + "Coadd_src"
+        dataset = "{}Coadd_{}".format(self.config.coaddName, self.datasetSuffix)
         tract = dataRef.dataId["tract"]
         butler = dataRef.butlerSubset.butler
         for patch in patchList:
