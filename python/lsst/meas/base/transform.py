69ca4d3f315dcb384e1fea2bf7c8947c51db6580
John Swinbank <swinbank@princeton.edu>
2015-01-29 12:34:21 -0500

    move wcs, calib args to __init__

--- python/lsst/meas/base/transform.py
+++ python/lsst/meas/base/transform.py
@@ -1,33 +1,33 @@
 class TransformPlugin(object):
-    def __init__(self, name, mapper, cfg, wcs, calib):
+    def __init__(self, name, mapper, cfg):
         self.name = name
         self.cfg = cfg
-        self.wcs = wcs
-        self.calib = calib
 
-    def __call__(self, oldRecord, newRecord):
+    def __call__(self, oldCatalog, newCatalog, wcs, calib):
         raise NotImplementedError()
 
 class NullTransform(TransformPlugin):
-    def __call__(self, oldRecord, newRecord):
+    def __call__(self, oldCatalog, newCatalog, wcs, calib):
         pass
 
 class PassThrough(TransformPlugin):
-    def __init__(self, name, mapper, cfg, wcs, calib):
-        TransformPlugin.__init__(self, name, mapper, cfg, wcs, calib)
+    def __init__(self, name, mapper, cfg):
+        TransformPlugin.__init__(self, name, mapper, cfg)
         for key, field in mapper.getInputSchema().extract(name + "*").itervalues():
             mapper.addMapping(key)
 
-    def __call__(self, oldRecord, newRecord):
+    def __call__(self, oldCatalog, newCatalog, wcs, calib):
         pass
 
 class ReverseCentroid(PassThrough):
-    def __init__(self, name, mapper, cfg, wcs, calib):
-        PassThrough.__init__(self, name, mapper, cfg, wcs, calib)
+    def __init__(self, name, mapper, cfg):
+        PassThrough.__init__(self, name, mapper, cfg)
         newSchema = mapper.editOutputSchema()
         self.keyRevX = newSchema.addField(self.name + "_revX", type="D", doc="reversed centroid", units="pixels")
         self.keyRevY = newSchema.addField(self.name + "_revY", type="D", doc="reversed centroid", units="pixels")
 
-    def __call__(self, oldRecord, newRecord):
-        newRecord.set(self.keyRevX, -1.0 * oldRecord['base_PeakCentroid_x'])
-        newRecord.set(self.keyRevY, -1.0 * oldRecord['base_PeakCentroid_y'])
+    def __call__(self, oldCatalog, newCatalog, wcs, calib):
+        oldColumns = oldCatalog.getColumnView()
+        newColumns = newCatalog.getColumnView()
+        newColumns[self.keyRevX] = -1.0 * oldColumns['base_PeakCentroid_x']
+        newColumns[self.keyRevY] = -1.0 * oldColumns['base_PeakCentroid_y']
