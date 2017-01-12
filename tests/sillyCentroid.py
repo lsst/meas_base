40287005a8a49ad64bc94a25a999cdf53634a266
Krzysztof Findeisen <kfindeis@uw.edu>
2017-01-12 13:28:08 -0800

    Allow transforms to take both Config and Control.
    This change does not affect meas_base unit tests, but is known to be needed by
    higher-level libraries.

--- tests/sillyCentroid.py
+++ tests/sillyCentroid.py
@@ -1,16 +1,9 @@
 from __future__ import absolute_import
 
-from lsst.meas.base import SimpleAlgorithm, wrapSimpleAlgorithm, BasePlugin
+from lsst.meas.base import SimpleAlgorithm, wrapSimpleAlgorithm, wrapTransform, BasePlugin
 from _sillyCentroid import SillyCentroidAlgorithm, SillyCentroidControl, SillyTransform
 
 # Do not register SillyCentroid in plugins.py, as it's not part of meas_base
 wrapSimpleAlgorithm(SillyCentroidAlgorithm, Control=SillyCentroidControl,
                     TransformClass=SillyTransform, executionOrder=BasePlugin.CENTROID_ORDER)
-
-# Emulate Swig's %feature("pythonprepend")
-SillyTransform.__init = SillyTransform.__init__
-def _init(self, ctrl, name, mapper):
-    if hasattr(ctrl, "makeControl"):
-        ctrl = ctrl.makeControl()
-    self.__init(ctrl, name, mapper)
-SillyTransform.__init__ = _init
+wrapTransform(SillyTransform)
