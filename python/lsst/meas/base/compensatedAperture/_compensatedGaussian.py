Fix normalization issues and do correct error calculation.

python/lsst/meas/base/compensatedAperture/_compensatedGaussian.py
@@ -28,7 +28,6 @@ class CompensatedGaussianAperturePluginConfig(CompensatedAperturePluginConfig):
         dtype=float,
         default=2,
     )
-    alpha = Field(doc="Approximate width of the psf if it were a symmetric gaussian", dtype=float, default=3)
 
 
 @register("base_CompensatedGaussianAp")
@@ -58,9 +57,9 @@ class CompensatedGaussianAperturePlugin(CompensatedAperturePlugin):
             flux_str = f"{base_key}_instFlux"
             flux_key = schema.addField(flux_str, type="D", doc="Compensated flux measurement")
 
-            # uncertainty
-            uncert_str = f"{base_key}_instFluxErr"
-            uncert_key = schema.addField(uncert_str, type="D", doc="Compensated flux uncertainty")
+            # flux error
+            err_str = f"{base_key}_instFluxErr"
+            err_key = schema.addField(err_str, type="D", doc="Compensated flux error")
 
             # mask bits
             mask_str = f"{base_key}_mask_bits"
@@ -70,17 +69,12 @@ class CompensatedGaussianAperturePlugin(CompensatedAperturePlugin):
             norm_str = f"{base_key}_normalization"
             norm_key = schema.addField(norm_str, type="D", doc="Compensated flux normalization; set later")
 
-            self.width_keys[width] = (flux_key, uncert_key, mask_key, norm_key)
+            self.width_keys[width] = (flux_key, err_key, mask_key, norm_key)
             self._rads[width] = math.ceil(sps.norm.ppf((0.995,), scale=width * config.t)[0])
 
-            alpha = self.config.alpha
-            self._flux_corrections[width] = (
-                2
-                * np.pi
-                * (alpha**4 + alpha**2 * width**2 * (self._t**2 + 1) + width**4 * self._t**2)
-                / (width**2 * (self._t**2 - 1))
-            )
-            self._variance_corrections[width] = 4 * np.pi * width**2 * (self._t**2 + 1) / self._t**2
+            # alpha = self.config.alpha
+            self._flux_corrections[width] = 4. * np.pi * width**2. * (self._t**2. + 1) / (self._t**2 - 1)
+            self._variance_corrections[width] = 4. * np.pi * width**2. * (self._t**2 + 1) / self._t**2.
 
         self._max_rad = max(self._rads)
 
@@ -96,16 +90,22 @@ class CompensatedGaussianAperturePlugin(CompensatedAperturePlugin):
         y_floor = math.floor(y)
         x_floor = math.floor(x)
 
-        for width, (flux_key, uncert_key, mask_key, norm_key) in self.width_keys.items():
+        for width, (flux_key, err_key, mask_key, norm_key) in self.width_keys.items():
             rad = self._rads[width]
             y_slice = slice(y_floor - rad, y_floor + rad + 1, 1)
             x_slice = slice(x_floor - rad, x_floor + rad + 1, 1)
             y_mean = y - y_floor + rad
             x_mean = x - x_floor + rad
-            flux_uncal, uncert_uncal = _gaussianFiltInnerProduct(
-                exposure.image.array[y_slice, x_slice], x_mean, y_mean, width, self._t
+
+            flux_uncal, var_uncal = _gaussianFiltInnerProduct(
+                exposure.image.array[y_slice, x_slice],
+                exposure.variance.array[y_slice, x_slice],
+                x_mean,
+                y_mean,
+                width,
+                self._t,
             )
             measRecord.set(flux_key, flux_uncal * self._flux_corrections[width])
-            measRecord.set(uncert_key, np.sqrt(uncert_uncal * self._variance_corrections[width]))
-            measRecord.set(mask_key, np.bitwise_or.reduce(exposure.mask.array, axis=None))
+            measRecord.set(err_key, np.sqrt(var_uncal * self._variance_corrections[width]))
+            measRecord.set(mask_key, np.bitwise_or.reduce(exposure.mask.array[y_slice, x_slice], axis=None))
             measRecord.set(norm_key, self._flux_corrections[width])