6cd13db3a308263191e16f5fd82c3e9373ab0510
Tim Jenness <tjenness@lsst.org>
2022-07-29 15:32:56 -0700

    Remove most of the content from references.py
    This is seemingly only used for gen2

--- python/lsst/meas/base/references.py
+++ python/lsst/meas/base/references.py
@@ -51,25 +51,14 @@ class BaseReferencesTask(lsst.pipe.base.Task):
 
     Parameters
     ----------
-    schema : `lsst.afw.table.Schema`, optional
+    schema : `lsst.afw.table.Schema`
         The schema of the reference catalog.
-    butler : `lsst.daf.persistence.butler.Butler`, optional
-        A butler that will allow the task to load its schema from disk.
+    butler : `None`.
+        Compatibility parameter. Should always be `None`.
 
     Notes
     -----
-    At least one of the ``schema`` and ``butler`` arguments must be present;
-    if both are, ``schema`` takes precedence.
-
-    ``BaseReferencesTask`` defines the required API for the references task,
-    which consists of:
-
-    - ``getSchema(butler)``
-    - ``fetchInPatches(butler, tract, filter, patchList)``
-    - ``fetchInBox(self, butler, tract, filter, bbox, wcs)``
-    - the ``removePatchOverlaps`` config option
-
-    It also provides the ``subset`` method, which may be of use to derived
+    Provides the ``subset`` method, which may be of use to derived
     classes when reimplementing ``fetchInBox``.
     """
 
@@ -78,86 +67,11 @@ class BaseReferencesTask(lsst.pipe.base.Task):
     """
 
     def __init__(self, butler=None, schema=None, **kwargs):
+        if butler is not None:
+            raise ValueError("Gen2 butler parameter is no longer supported.")
+        if schema is None:
+            raise ValueError("A schema parameter must be provided.")
         lsst.pipe.base.Task.__init__(self, **kwargs)
-
-    def getSchema(self, butler):
-        """Return the schema for the reference sources.
-
-        Parameters
-        ----------
-        butler : `lsst.daf.persistence.butler.Butler`
-            Data butler from which the schema will be fetched.
-
-        Notes
-        -----
-        Must be available even before any data has been processed.
-        """
-        raise NotImplementedError("BaseReferencesTask is pure abstract, and cannot be used directly.")
-
-    def getWcs(self, dataRef):
-        """Return the WCS for reference sources.
-
-        Parameters
-        ----------
-        dataRef : `lsst.daf.persistence.ButlerDataRef`
-            The data reference from which the WCS will be fetched. This must
-            include the tract in its dataId.
-        """
-        raise NotImplementedError("BaseReferencesTask is pure abstract, and cannot be used directly.")
-
-    def fetchInBox(self, dataRef, bbox, wcs):
-        """Return reference sources within a given bounding box.
-
-        Reference sources are selected if they overlap a region defined by a
-        pixel-coordinate bounding box and corresponding WCS.
-
-        Parameters
-        ----------
-        dataRef : `lsst.daf.persistence.ButlerDataRef`
-            Butler data reference. The implied data ID must contain the
-            ``tract`` key.
-        bbox : `lsst.afw.geom.Box2I` or `lsst.afw.geom.Box2D`
-            Defines the selection region in pixel coordinates.
-        wcs : `lsst.afw.image.SkyWcs`
-            Maps ``bbox`` to sky coordinates.
-
-        Returns
-        -------
-        sources : iterable of `~lsst.afw.table.SourceRecord`
-            Reference sources. May be any Python iterable, including a lazy
-            iterator.
-
-        Notes
-        -----
-        The returned set of sources should be complete and close to minimal.
-        """
-        raise NotImplementedError("BaseReferencesTask is pure abstract, and cannot be used directly.")
-
-    def fetchInPatches(self, dataRef, patchList):
-        """Return reference sources within one or more patches.
-
-        Parameters
-        ----------
-        dataRef : `lsst.daf.persistence.ButlerDataRef`
-            Butler data reference. The implied data ID must contain the
-            ``tract`` key.
-        patchList : `list` of `lsst.skymap.PatchInfo`
-            Patches for which to fetch reference sources.
-
-        Returns
-        -------
-        sources : iterable of `~lsst.afw.table.SourceRecord`
-            Reference sources. May be any Python iterable, including a lazy
-            iterator.
-
-        Notes
-        -----
-        The returned set of sources should be complete and close to minimal.
-
-        If ``config.removePatchOverlaps`` is `True`, only sources within each
-        patch's "inner" bounding box should be returned.
-        """
-        raise NotImplementedError("BaseReferencesTask is pure abstract, and cannot be used directly.")
 
     def subset(self, sources, bbox, wcs):
         """Filter a list of sources to only those within the bounding box.
@@ -244,9 +158,8 @@ class CoaddSrcReferencesTask(BaseReferencesTask):
     ----------
     schema : `lsst.afw.table.Schema`, optional
         The schema of the detection catalogs used as input to this one.
-    butler : `lsst.daf.persistence.butler.Butler`, optional
-        A Butler used to read the input schema from disk. Required if
-        ``schema`` is `None`.
+    butler : `None`
+        Compatibility parameter. Should always be `None`.
 
     Notes
     -----
@@ -264,104 +177,7 @@ class CoaddSrcReferencesTask(BaseReferencesTask):
 
     def __init__(self, butler=None, schema=None, **kwargs):
         BaseReferencesTask.__init__(self, butler=butler, schema=schema, **kwargs)
-        if schema is None:
-            assert butler is not None, "No butler nor schema provided"
-            schema = butler.get("{}Coadd_{}_schema".format(self.config.coaddName, self.datasetSuffix),
-                                immediate=True).getSchema()
         self.schema = schema
-
-    def getWcs(self, dataRef):
-        """Return the WCS for reference sources.
-
-        Parameters
-        ----------
-        dataRef : `lsst.daf.persistence.ButlerDataRef`
-            Butler data reference. Must includ the trac in its dataId.
-        """
-        skyMap = dataRef.get(self.config.coaddName + "Coadd_skyMap", immediate=True)
-        return skyMap[dataRef.dataId["tract"]].getWcs()
-
-    def fetchInPatches(self, dataRef, patchList):
-        """Fetch the source catalog using the Butler.
-
-        Parameters
-        ----------
-        dataRef : `lsst.daf.persistence.ButlerDataRef`
-            Butler data reference. The implied data ID must contain the
-            ``tract`` key.
-        patchList : `list` of `lsst.skymap.PatchInfo`
-            Patches for which to fetch reference sources.
-
-        Returns
-        -------
-        sources : iterable of `~lsst.afw.table.SourceRecord`
-            Reference sources. May be any Python iterable, including a lazy
-            iterator.
-
-        Notes
-        -----
-        An implementation of `BaseReferencesTask.fetchInPatches` that loads
-        ``Coadd_`` + `datasetSuffix` catalogs using the butler.
-        """
-        dataset = "{}Coadd_{}".format(self.config.coaddName, self.datasetSuffix)
-        tract = dataRef.dataId["tract"]
-        butler = dataRef.butlerSubset.butler
-        for patch in patchList:
-            dataId = {'tract': tract, 'patch': "%d,%d" % patch.getIndex()}
-            if self.config.filter is not None:
-                dataId['filter'] = self.config.filter
-
-            if not butler.datasetExists(dataset, dataId):
-                if self.config.skipMissing:
-                    continue
-                raise lsst.pipe.base.TaskError("Reference %s doesn't exist" % (dataId,))
-            self.log.info("Getting references in %s", dataId)
-            catalog = butler.get(dataset, dataId, immediate=True)
-            if self.config.removePatchOverlaps:
-                bbox = lsst.geom.Box2D(patch.getInnerBBox())
-                for source in catalog:
-                    if bbox.contains(source.getCentroid()):
-                        yield source
-            else:
-                for source in catalog:
-                    yield source
-
-    def fetchInBox(self, dataRef, bbox, wcs, pad=0):
-        """Return reference sources within a given bounding box.
-
-        Reference sources are selected if they overlap a region defined by a
-        pixel-coordinate bounding box and corresponding WCS.
-
-        Parameters
-        ----------
-        dataRef : `lsst.daf.persistence.ButlerDataRef`
-            Butler data reference. The implied data ID must contain the
-            ``tract`` key.
-        bbox : `lsst.afw.geom.Box2I` or `lsst.afw.geom.Box2D`
-            Defines the selection region in pixel coordinates.
-        wcs : `lsst.afw.image.SkyWcs`
-            Maps ``bbox`` to sky coordinates.
-        pad : `int`
-            a buffer to grow the bounding box by after catalogs have been loaded, but
-            before filtering them to include just the given bounding box.
-
-        Returns
-        -------
-        sources : iterable of `~lsst.afw.table.SourceRecord`
-            Reference sources. May be any Python iterable, including a lazy
-            iterator.
-        """
-        skyMap = dataRef.get(self.config.coaddName + "Coadd_skyMap", immediate=True)
-        tract = skyMap[dataRef.dataId["tract"]]
-        coordList = [wcs.pixelToSky(corner) for corner in lsst.geom.Box2D(bbox).getCorners()]
-        self.log.info("Getting references in region with corners %s [degrees]",
-                      ", ".join("(%s)" % (coord.getPosition(lsst.geom.degrees),) for coord in coordList))
-        patchList = tract.findPatchList(coordList)
-        # After figuring out which patch catalogs to read from the bbox, pad out the bbox if desired
-        # But don't add any new patches while padding
-        if pad:
-            bbox.grow(pad)
-        return self.subset(self.fetchInPatches(dataRef, patchList), bbox, wcs)
 
 
 class MultiBandReferencesConfig(CoaddSrcReferencesTask.ConfigClass):
